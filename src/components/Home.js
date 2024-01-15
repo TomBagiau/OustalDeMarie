07044ad76de434c066952b342241bda695136994
Tom <39921579+TomBagiau@users.noreply.github.com>
2024-01-15 12:46:24 +0100

    baniere home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Helmet } from "react-helmet";
 import background from "../img/home_background.JPG"
+import banniere from "../img/accueil-oustal-de-marie.JPG"
 import Cards from "./Cards";
 import Maps from "./Maps";
 import "./Home.css"
@@ -25,7 +26,7 @@ export default function Home() {
 
       <section className="homeBaniere">
         <div className="relative w-full">
-          <img src="https://images.unsplash.com/photo-1569077251332-3c0293ae7d12?auto=format&fit=crop&q=80&w=2070&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Gîte en ariège" className="object-cover w-full banner"/>
+          <img src={banniere} alt="Gîte en ariège" className="object-cover w-full banner"/>
           <div className="absolute inset-0 flex items-center justify-center">
             <p className="text-4xl text-white font-bold">Visiter l'ariège avec L'Oustal de Marie</p>
           </div>
