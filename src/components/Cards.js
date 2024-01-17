0ea1bf1b2e9a20498a77aefe20b3de1fa5913233
Tom <39921579+TomBagiau@users.noreply.github.com>
2024-01-17 16:19:09 +0100

    bouton lire plus cartes

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -14,6 +14,16 @@ import "./Home.css";
 export default function Cards() {
   const [cardData, setCardData] = useState([]);
 
+  const characterLimit = 277;
+
+  const [showFullTextArray, setShowFullTextArray] = useState(Array(cardData.length).fill(false));
+
+  const handleToggleText = (index) => {
+    const newShowFullTextArray = [...showFullTextArray];
+    newShowFullTextArray[index] = !newShowFullTextArray[index];
+    setShowFullTextArray(newShowFullTextArray);
+  };
+
   useEffect(() => {
     /**
      * Sanity cartes
@@ -37,30 +47,38 @@ export default function Cards() {
 
   return (
     <div className="flex flex-wrap justify-around items-center mx-auto center">
-    {cardData &&
-    cardData.map((card, index) => (
-      <Card key={index} className="sm:w-1/2 md:w-1/2 lg:w-1/4 xl:w-1/4 my-2 mx-2 mt-10 cart text-white">
-        <CardHeader color="blue-gray" className="relative h-72">
-          <img
-            src={card.image.asset.url} // Accédez à l'URL de l'image via card.image.asset.url
-            alt="card-img"
-          />
-        </CardHeader>
-        <CardBody>
-          <Typography variant="h3" color="blue-gray" className="mb-2">
-            {card.titre}
-          </Typography>
-          <Typography>
-            {card.description}
-          </Typography>
-        </CardBody>
-        <CardFooter className="pt-0">
-          <Link to={card.lien}>
-            <Button className="button">Voir plus</Button>
-          </Link>
-        </CardFooter>
-      </Card>
-    ))}
-  </div>
+      {cardData &&
+        cardData.map((card, index) => (
+          <Card key={index} className="sm:w-1/2 md:w-1/2 lg:w-1/4 xl:w-1/4 my-2 mx-2 mt-10 cart text-white">
+            <CardHeader color="blue-gray" className="relative h-72">
+              <img
+                src={card.image.asset.url}
+                alt="card-img"
+              />
+            </CardHeader>
+            <CardBody>
+              <Typography variant="h3" color="blue-gray" className="mb-2">
+                {card.titre}
+              </Typography>
+              <Typography>
+                {showFullTextArray[index] || card.description.length <= characterLimit
+                  ? card.description
+                  : `${card.description.slice(0, characterLimit)}...`}
+                {card.description.length > characterLimit && (
+                  // eslint-disable-next-line jsx-a11y/anchor-is-valid
+                  <a className="cursor-pointer readmore" onClick={() => handleToggleText(index)}>
+                    {showFullTextArray[index] ? ' Lire moins' : ' Lire plus'}
+                  </a>
+                )}
+              </Typography>
+            </CardBody>
+            <CardFooter className="pt-0">
+              <Link to={card.lien}>
+                <Button className="button">Voir plus</Button>
+              </Link>
+            </CardFooter>
+          </Card>
+        ))}
+    </div>
   );
 }
